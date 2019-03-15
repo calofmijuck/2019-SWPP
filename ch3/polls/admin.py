2af289efc87930405968ab61688704d9f45fd02b
Sungchan Yi <calofmijuck@snu.ac.kr>
2019-03-15 10:51:01 +0900

    Django Book 4.1

--- ch3/polls/admin.py
+++ ch3/polls/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
-admin.site.register(Question)
+# class ChoiceInline(admin.StackedInline): # Modify question and choice at once
+class ChoiceInline(admin.TabularInline): # show as table
+    model = Choice
+    extra = 2 # Additional two as default
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [ # Separae each field and show
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}), # collapse
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date') # Set record list column
+    list_filter = ['pub_date'] # filter side bar
+    search_fields = ['question_text'] # Search box
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
