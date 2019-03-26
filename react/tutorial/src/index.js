bf24955a6cfccbb905c4b00dd5a14b38286901d3
Sungchan Yi <calofmijuck@snu.ac.kr>
2019-03-26 22:24:39 +0900

    Example

--- react/tutorial/src/index.js
+++ react/tutorial/src/index.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-unused-expressions */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ShoppingList from './ShoppingList'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// <ShoppingList name="sung"/>
+ReactDOM.render(<ShoppingList name="Sung"/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
