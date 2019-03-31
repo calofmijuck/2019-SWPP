297866deae5d1072a27e9f8818e675afcc637788
Sungchan Yi <calofmijuck@snu.ac.kr>
2019-03-31 18:25:46 +0900

    JSONParser

--- meeting/meeting_back/hw1_1/settings.py
+++ meeting/meeting_back/hw1_1/settings.py
@@ -73,7 +73,8 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.FormParser',
-        'rest_framework.parsers.MultiPartParser'
+        'rest_framework.parsers.MultiPartParser',
+        'rest_framework.parsers.JSONParser'
     )
 }
 
